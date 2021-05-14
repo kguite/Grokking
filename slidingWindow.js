6752fa6ca4b6e9be122adcbb731025c6037ff12b
kguite <kguite@gmail.com>
2021-05-13 21:07:55 -0700

    sliding window

--- slidingWindow.js
+++ slidingWindow.js
@@ -17,3 +17,6 @@ function find_averages_of_subarrays(K, arr) {
   
   const result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
   console.log(`Averages of subarrays of size K: ${result}`);
+  
+  // think of a caterpillar
+  // netflix 
